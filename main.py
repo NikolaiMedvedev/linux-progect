74ce74fa137eeeded10c85e12d05bff2d1326613
Dimker <kleshnin838@mail.ru>
2024-12-01 13:00:18 +0500

    Сделал проверку на победителей. Дмитрий

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Сделал доску
 def print_board(board):
     print("Доска:")
     display_board = []
@@ -9,3 +10,24 @@ def print_board(board):
         display_board.append(" | ".join(row))
     print("\n".join(display_board))
     print("-" * 9)
+
+
+# Сделал проверку на победителей
+def check_winner(board):
+    # Проверка строк
+    for row in board:
+        if row[0] == row[1] == row[2] != " ":
+            return row[0]
+
+    # Проверка столбцов
+    for col in range(3):
+        if board[0][col] == board[1][col] == board[2][col] != " ":
+            return board[0][col]
+
+    # Проверка диагоналей
+    if board[0][0] == board[1][1] == board[2][2] != " ":
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] != " ":
+        return board[0][2]
+
+    return None
